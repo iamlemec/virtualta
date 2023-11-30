565a66a29b20c3e1080badb01fa97a50b6621f78
Douglas Hanley <thesecretaryofwar@gmail.com>
2023-11-30 02:50:39 -0500

    revamp look

--- app.py
+++ app.py
@@ -1,74 +1,38 @@
-import os
-import time
-import openai
+# chat bot
 
-from openai import OpenAI
-from flask import Flask, render_template, request, jsonify, send_file
+import toml
+from flask import Flask, render_template, request, jsonify
 
-thread_id='thread_Ge20kslclNBsNheDJXcMDNnq'
-assist_id= 'asst_oRP7qsyru69o9LtYNlAP00Bt'
+import messages
 
+# create app
 app = Flask(
-    __name__,
-    static_folder='static',
-    template_folder='templates'
+    __name__, static_folder='static', template_folder='templates'
 )
 
-client = openai.OpenAI()
+# create client
+conf = toml.load('config.toml')
+client = messages.MessageQueue(**conf)
 
+# routes
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/get_response', methods=['POST'])
-def get_response():
-    user_message = request.form['user_message']
-    response = generate_chat_response(user_message)
-    return jsonify(response)
+@app.route('/list', methods=['POST'])
+def list():
+    print('list')
+    client.update()
+    return jsonify(client.queue)
 
-def generate_chat_response(user_message):
-    # create message
-    response = openai.beta.threads.messages.create(
-        thread_id=thread_id,
-        role="user",
-        content=user_message,
-    )
+@app.route('/query', methods=['POST'])
+def query():
+    prompt = request.form['prompt']
+    print('query:', prompt)
+    client.query(prompt, block=True)
+    client.update()
+    return jsonify(client.queue)
 
-    # run assistant
-    run = client.beta.threads.runs.create(
-        thread_id=thread_id,
-        assistant_id=assist_id,
-    )
-
-    # wait for completion
-    for _ in range(20):
-        # get run status
-        run = client.beta.threads.runs.retrieve(
-            thread_id=thread_id,
-            run_id=run.id
-        )
-
-        # check for completion
-        print(run)
-        if run.status == 'completed':
-            break
-        else:
-            time.sleep(2)
-
-
-    # get messages
-    messages = client.beta.threads.messages.list(
-        thread_id=thread_id
-    )
-
-    # parse messages
-    print(messages)
-    contents = [
-        [c.text.value for c in d.content]
-        for d in messages.data
-    ]
-    print(contents)
-    return contents
-
+# main loop
 if __name__ == '__main__':
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -1,56 +1,90 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sendButton = document.querySelector('.entry-box button');
-    const messageInput = document.querySelector('.entry-box input');
-    const chatBox = document.querySelector('.chat-box');
+// chat bot interface
 
-    sendButton.addEventListener('click', function() {
-        const message = messageInput.value.trim();
-        if (message) {
-            sendMessage(message);
-            messageInput.value = ''; // Clear the input field
+document.addEventListener('DOMContentLoaded', async () => {
+    const send = document.querySelector('.entry-box button');
+    const input = document.querySelector('.entry-box input');
+    const chat = document.querySelector('.chat-box');
+
+    // add to chat box
+    function appendMessage(data) {
+        for ([id, role, text] of data) {
+            let outer = document.createElement('div');
+            let inner = document.createElement('div');
+            outer.setAttribute('message-id', id);
+            outer.classList.add('message');
+            outer.classList.add(role);
+            inner.innerText = text;
+            outer.appendChild(inner);
+            chat.appendChild(outer);
+        }
+    }
+    
+    // history fetcher
+    async function populateHistory() {
+        // initiate request
+        let response = await fetch('/list', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        });
+
+        // check return status
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        // convert response to json
+        let data = await response.json();
+        console.log(data);
+        appendMessage(data);
+    }
+
+    // send the message to the server
+    async function sendMessage(prompt) {
+        // initiate request
+        let response = await fetch('/query', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: new URLSearchParams({prompt}),
+        });
+
+        // check return status
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        // convert response to json
+        let data = await response.json();
+        console.log(data);
+        appendMessage(data);
+    }
+
+    // handle message
+    async function handleMessage() {
+        let prompt = input.value.trim();
+        if (prompt.length > 0) {
+            send.disabled = true;
+            input.disabled = true;
+            await sendMessage(prompt);
+            input.value = '';
+            chat.scrollTop = chat.scrollHeight;
+            send.disabled = false;
+            input.disabled = false;
+        }
+    }
+
+    // connect handler
+    send.addEventListener('click', () => {
+        handleMessage();
+    });
+
+    // enter handler
+    input.addEventListener('keydown', (event) => {
+        if (event.keyCode === 13) {
+            handleMessage();
         }
     });
 
-    function sendMessage(message) {
-        // Send the message to the server
-        fetch('/get_response', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({
-                'user_message': message,
-            }),
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(responseMessage => {
-            // Handle the response from the server
-            appendMessage(responseMessage);
-        })
-        .catch(error => {
-            console.error('Fetch error: ', error);
-        });
-    }
-
-    function appendMessage(data) {
-        // Display the bot's response
-        console.log(data);
-
-        // turn list of lists into divs
-        for (row of data.toReversed()) {
-            for (text of row) {
-                let col = document.createElement('div');
-                col.innerText = text;
-                chatBox.appendChild(col);
-            }
-        }
-
-        // Scroll to the bottom
-        chatBox.scrollTop = chatBox.scrollHeight;
-    }
+    // populate history
+    await populateHistory();
+    chat.scrollTop = chat.scrollHeight;
 });
